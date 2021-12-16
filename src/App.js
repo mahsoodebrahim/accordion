9b6c5f1af441cebf20afe8b810eb28c2eaa7a097
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2021-12-16 11:49:48 -0800

    added toggle functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import data from "./data";
 import SingleQuestion from "./Question";
 
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>FAQ</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion {...question} key={question.id} />;
+          })}
+        </section>
+      </section>
+    </main>
+  );
 }
 
 export default App;
